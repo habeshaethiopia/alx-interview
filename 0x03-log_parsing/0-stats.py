22734f247a3a23857f9f435cfe52e73338e76aa7
Adane <adanemoges6@gmail.com>
2024-01-25 16:03:39 +0300

    trial 2

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -10,17 +10,17 @@ dic = {}
 while True:
     try:
         s = input().split()
-
-        dic[int(s[-2])] = 1 if int(s[-2]) not in dic else dic[int(s[-2])] + 1
-        l = len(s)
-
-        size += int(s[l - 1])
+        length = len(s)
+        size += int(s[length - 1])
         if c % 10 == 0:
             if dic:
                 for key, value in sorted(dic.items()):
                     print("{}: {}".format(key, value))
             print("File size: {}".format(size))
             dic.clear()
+
+        dic[int(s[-2])] = 1 if int(s[-2]) not in dic else dic[int(s[-2])] + 1
+
         c += 1
     except KeyboardInterrupt:
         print("File size: {}".format(size))
