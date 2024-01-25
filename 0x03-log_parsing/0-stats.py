71b1b5fd72f5230292e8a46b4ac79f2887700f8c
Adane <adanemoges6@gmail.com>
2024-01-25 16:10:44 +0300

    trial 3

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -4,23 +4,29 @@ and computes metrics"""
 import sys
 
 # from collections import defaultdict
-size = 0
-c = 0
-dic = {}
-while True:
-    try:
-        s = input().split()
-        length = len(s)
-        size += int(s[length - 1])
-        if c % 10 == 0:
-            if dic:
-                for key, value in sorted(dic.items()):
-                    print("{}: {}".format(key, value))
+def print_stats()-> None:
+    """print stats"""
+    
+    size = 0
+    c = 0
+    dic = {}
+    status = [200, 301, 400, 401, 403, 404, 405, 500]
+    while True:
+        try:
+            s = input().split()
+            length = len(s)
+            size += int(s[length - 1])
+            if c % 10 == 0:
+                if dic:
+                    for key, value in sorted(dic.items()):
+                        print("{}: {}".format(key, value))
+                print("File size: {}".format(size))
+                dic.clear()
+            if int(s[-2]) in status:
+                dic[int(s[-2])] = 1 if int(s[-2]) not in dic else dic[int(s[-2])] + 1
+
+            c += 1
+        except KeyboardInterrupt:
             print("File size: {}".format(size))
-            dic.clear()
-
-        dic[int(s[-2])] = 1 if int(s[-2]) not in dic else dic[int(s[-2])] + 1
-
-        c += 1
-    except KeyboardInterrupt:
-        print("File size: {}".format(size))
+if __name__ == "__main__":
+    print_stats()
