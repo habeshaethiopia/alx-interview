4bf4731f77a3b1f38c70d2ccba187b7c425069b6
Adane <adanemoges6@gmail.com>
2024-02-08 20:10:40 +0300

    ;astly

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,12 +1,41 @@
 #!/usr/bin/python3
 """interview question n queens"""
 import sys
-from itertools import permutations
-n = int(sys.argv[1])
-print([
-            ["." * i + "Q" + "." * (n - i - 1) for i in p]
-            for p in permutations(range(n))
-            if len(set(map(lambda z: z[0] - z[1], enumerate(p))))
-            == len(set(map(lambda z: z[0] + z[1], enumerate(p))))
-            == n
-        ])
+
+if len(sys.argv) != 2:
+    print("Usage: nqueens N")
+    sys.exit(1)
+n = sys.argv[1]
+if not n.isdigit():
+    print("N must be a number")
+    sys.exit(1)
+n = int(n)
+if n < 4:
+    print("N must be at least 4")
+    sys.exit(1)
+
+
+def queens(n, i=0, a=[], b=[], c=[]):
+    """find possible positions"""
+    if i < n:
+        for j in range(n):
+            if j not in a and i + j not in b and i - j not in c:
+                yield from queens(n, i + 1, a + [j], b + [i + j], c + [i - j])
+    else:
+        yield a
+
+
+def solve(n):
+    """solve"""
+    k = []
+    i = 0
+    for solution in queens(n, 0):
+        for s in solution:
+            k.append([i, s])
+            i += 1
+        print(k)
+        k = []
+        i = 0
+
+
+solve(n)
