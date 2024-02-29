2dfca6bf97f8643b5a35b0d82466154968ef56c0
Adane <adanemoges6@gmail.com>
2024-02-29 22:50:44 +0300

    dp

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -3,67 +3,19 @@
 minimum number of coins required
 to make up a given total amount
 """
-import sys
+
+from typing import List
 
 
-def makeChange(coins, amount):
-    """
-    :type coins: list of int
-    :type amount: int
-    :rtype: int
-    """
-    if amount < 1:
-        return 0
+def makeChange(coins: List[int], amount: int) -> int:
+    """make changes"""
+    dp = [0] + [float("inf")] * amount
+    for i in range(1, amount + 1):
+        for j in coins:
+            if i >= j:
+                dp[i] = min(dp[i - j] + 1, dp[i])
 
-    return coin_change(coins, amount, [0] * (amount + 1))
-
-
-def coin_change(coins, remainder, cache):
-    """
-    Minimum coins to make change for a negative amount is -1.
-    This is just a base case we arbitrarily define.
-    """
-    if remainder < 0:
-        return -1
-
-    """
-        The minimum coins needed to make change for 0 is always 0
-        coins no matter what coins we have.
-        """
-    if remainder == 0:
-        return 0
-
-    # We already have an answer cached. Return it.
-    if cache[remainder] != 0:
-        return cache[remainder]
-
-    # No answer yet. Try each coin as the last coin in the change that
-    # we make for the amount
-    system_max = sys.maxsize
-    minimum = system_max
-    for coin in coins:
-        change_result = coin_change(coins, remainder - coin, cache)
-
-        """
-            If making change was possible (changeResult >= 0) and
-            the change result beats our present minimum, add one to
-            that smallest value
-
-            We accept that coin as the last coin in our change making
-            sequence up to this point since it minimizes the coins we
-            need
-            """
-        if change_result >= 0 and change_result < minimum:
-            minimum = 1 + change_result
-
-    """
-        If no answer is found (minimum == max value) then the
-        sub problem answer is just arbitrarily made to be -1, otherwise
-        the sub problem's answer is "minimum"
-        """
-    cache[remainder] = -1 if (minimum == system_max) else minimum
-
-    return cache[remainder]
+    return dp[amount] if dp[amount] != float("inf") else -1
 
 
-# print(makeChange([1,2,3], 10))
+# print(makeChange([1256, 54, 48, 16, 102], 11111))
